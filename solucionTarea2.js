a185cd436085ea071a110d05783db20f7c71cb95
KiraElric <lissette.urso@gmail.com>
2022-02-03 18:57:42 -0300

    Fix Time Zone offset

--- solucionTarea2.js
+++ solucionTarea2.js
@@ -11,13 +11,13 @@ const rl = reader.createInterface({
 //Se solicita el inicio del intervalo
 rl.question("¿Ingrese la fecha de inicio del periodo de busqueda (AAAA/MM/DD)? ", (initDate) => {
   //Se transforma a formato Date
-  const startDate = new Date(initDate);
+  const startDate = new Date(initDate.replace('-','/'));
   //Se valida que sea una fecha valida
   if (isValid(startDate)) {
     //Se solicita el final del intervalo
     rl.question("¿Ingrese la fecha de termino del periodo de busqueda (AAAA/MM/DD)? ", (endDate) => {
       //Se transforma a formato Date
-      const finalDate = new Date(endDate);
+      const finalDate = new Date(endDate.replace('-','/'));
       //Se valida que sea una fecha valida
       if (isValid(finalDate)) {
         //Se valida que el inicio del intervalo sea menor que el final del intervalo
@@ -25,21 +25,25 @@ rl.question("¿Ingrese la fecha de inicio del periodo de busqueda (AAAA/MM/DD)? 
           console.log('----- Listado de cumpleaños -----');
           //Se llama la funcion del archivo app para mostrar los cumpleaños en el intervalo solicitado
           appFunction.birthday(startDate,finalDate);
+          rl.close();
         }
         else if (compareDesc(startDate, finalDate) === -1){
           console.log('El intervalo no es el adecuado');
+          rl.close();
         } 
         else {
           console.log('El inicio y termino del intervalo es el mismo día')
+          rl.close();
         }
       }
       else {
         console.error('La fecha ingresada no es valida.');
+        rl.close();
       }
     })
   } else {
     console.error('La fecha ingresada no es valida');
+    rl.close();
   }
-  rl.close();
 });
   
