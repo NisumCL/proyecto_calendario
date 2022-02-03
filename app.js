33cb7ec16440c8d638d575338cd40e33836fa08a
KiraElric <lissette.urso@gmail.com>
2022-02-03 18:44:49 -0300

    Solution of second task

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-const parse = require('csv-parser')
-const fs = require('fs')
+const parse = require('csv-parser');
+const fs = require('fs');
 
-const fecha1 = new Date(2021,1,28)
-const fecha2 = new Date(fecha1.getFullYear(), fecha1.getMonth(), fecha1.getDate()+15)
+//const fecha1 = new Date(2021,1,28)
+//const fecha2 = new Date(fecha1.getFullYear(), fecha1.getMonth(), fecha1.getDate()+15)
 
-const cumpleanios = []
+const cumpleanios = [];
 
 // Enumero a los cumpleañeros y muestro solo su fecha de nacimiento y su nombre
 const mostrar = (datos) =>{
     for (i in datos){
-        var a=parseInt(i)+1
-        console.log(a,".- ",datos[i].cumpleanios, datos[i].apellido_y_nombre)
+        var a=parseInt(i)+1;
+        console.log(a,".- ",datos[i].cumpleanios, datos[i].apellido_y_nombre);
     }
-}
+};
 
 // Recibo ambas fechas en formato Date() mas el cumpleaños en formato de lista [aaaa,mm,dd], 
 // retorno true si el cumpleaños esta entre la fecha1 y la fecha2
 // retorno false en otro caso
 const compararFecha = (fecha1, fecha2, cumple) => {
-    mesCumple = parseInt(cumple[1])
-    diaCumple= parseInt(cumple[2])
+    mesCumple = parseInt(cumple[1]);
+    diaCumple= parseInt(cumple[2]);
     if(mesCumple == fecha1.getMonth()+1 && mesCumple == fecha2.getMonth()+1){
         if (diaCumple >= fecha1.getDate() && diaCumple <= fecha2.getDate()){
-            return true
+            return true;
         }
     }
     else if(mesCumple === fecha1.getMonth()+1){
         if(diaCumple >= fecha1.getDate()){
-            return true
+            return true;
         }
     }else if (mesCumple === fecha2.getMonth()+1){
         if(diaCumple <= fecha2.getDate()){
-            return true
+            return true;
         }
     }
-    return false
+    return false;
 }
 
 // Leo el archivo .csv y guardo solo los empleados que estan de cumpleaños
 // desde hoy hasta 15 dias mas
-fs.createReadStream('mails_y_cumples_03.csv')
+function birthday(fecha1, fecha2) {
+    fs.createReadStream('mails_y_cumples_03.csv')
     .pipe(parse({
             delimiter: ','
         })
@@ -51,4 +52,7 @@ fs.createReadStream('mails_y_cumples_03.csv')
     })
     .on("end",() => {
         mostrar(cumpleanios)
-    });
+    })
+};
+
+module.exports = { birthday };
---
+++ solucionTarea2.js
@@ -0,0 +1,45 @@
+const reader = require('readline');
+const isValid = require('date-fns/isValid');
+const compareDesc = require('date-fns/compareDesc');
+const appFunction = require('./app.js');
+
+const rl = reader.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+//Se solicita el inicio del intervalo
+rl.question("¿Ingrese la fecha de inicio del periodo de busqueda (AAAA/MM/DD)? ", (initDate) => {
+  //Se transforma a formato Date
+  const startDate = new Date(initDate);
+  //Se valida que sea una fecha valida
+  if (isValid(startDate)) {
+    //Se solicita el final del intervalo
+    rl.question("¿Ingrese la fecha de termino del periodo de busqueda (AAAA/MM/DD)? ", (endDate) => {
+      //Se transforma a formato Date
+      const finalDate = new Date(endDate);
+      //Se valida que sea una fecha valida
+      if (isValid(finalDate)) {
+        //Se valida que el inicio del intervalo sea menor que el final del intervalo
+        if (compareDesc(startDate, finalDate) === 1){
+          console.log('----- Listado de cumpleaños -----');
+          //Se llama la funcion del archivo app para mostrar los cumpleaños en el intervalo solicitado
+          appFunction.birthday(startDate,finalDate);
+        }
+        else if (compareDesc(startDate, finalDate) === -1){
+          console.log('El intervalo no es el adecuado');
+        } 
+        else {
+          console.log('El inicio y termino del intervalo es el mismo día')
+        }
+      }
+      else {
+        console.error('La fecha ingresada no es valida.');
+      }
+    })
+  } else {
+    console.error('La fecha ingresada no es valida');
+  }
+  rl.close();
+});
+  
