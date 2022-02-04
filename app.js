1c118f0ed6fcb428b67f9f9a0cb259a337dcf280
Beatriz <lopezcasanovabeatriz@gmail.com>
2022-02-04 10:44:55 -0300

    Version final

--- app.js
+++ app.js
@@ -3,18 +3,69 @@ const fs = require("fs");
 
 // Ingresar fechas por consola
 const startDate = process.argv[2];
-const limitDate = process.argv[3];
+const finishDate = process.argv[3];
 
 const fecha1 = new Date(startDate);
-const fecha2 = new Date(limitDate);
+const fecha2 = new Date(finishDate);
 
-// Es para validar que la primera fecha no sea mayor que la segunda.
-let isValidDate = false;
-
-if (fecha1 <= fecha2) {
-  isValidDate = true;
+// Valida que no esten vacías las fechas ingresadas
+if (!startDate || !finishDate) {
+  console.log("Ingresa una fecha");
 }
 
+// Formato de Fecha (reutilizado de StackOverFlow)
+let dateFormat = (dateString) => {
+  // First check for the pattern
+  let regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
+  //console.log("dateString", dateString);
+
+  if (!regex_date.test(dateString)) {
+    return false;
+  }
+
+  // Parse the date parts to integers
+  let parts = dateString.split("-");
+  let day = parseInt(parts[2], 10);
+  let month = parseInt(parts[1], 10);
+  let year = parseInt(parts[0], 10);
+  // console.log("year", year);
+  // console.log("dia", day);
+  // console.log("mes", month);
+
+  // Check the ranges of month and year
+  if (year < 1000 || year > 3000 || month == 0 || month > 12) {
+    return false;
+  }
+
+  let monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  // Adjust for leap years
+  if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+    monthLength[1] = 29;
+  }
+
+  // Check the range of the day
+  return day > 0 && day <= monthLength[month - 1];
+};
+// console.log(dateFormat(startDate));
+// console.log(dateFormat(finishDate));
+
+// Valida que cumpla el formato de la variable "dateFormat"......
+if (!dateFormat(startDate) && !dateFormat(finishDate)) {
+  console.log('Debes ingresar el formato de fecha: "YYYY-MM-DD"');
+}
+
+// Valida que la primera fecha no sea mayor que la segunda
+let isValidDate = false;
+if (fecha1 <= fecha2) {
+  isValidDate = true;
+} else {
+  console.log(
+    "La primera fecha debe ser menor a la segunda, ingresa una nueva fecha"
+  );
+}
+
+// Lista los cumpleaños que cumplen las condiciones anteriores
 if (isValidDate) {
   const cumpleanios = [];
 
