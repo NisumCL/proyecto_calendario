95441da82dbc0728d498be137c406cb227248cdd
javierast23 <84755115+javierast23@users.noreply.github.com>
2022-02-10 00:26:49 -0300

    busca entre rango de fechas (#11)
    * busca entre rango de fechas
    
    * comentarios eliminados
    
    * proyecto con los cambios sugeridos
    
    * cambios en los else if

--- app.js
+++ app.js
@@ -1,44 +1,59 @@
 const parse = require('csv-parser')
 const fs = require('fs')
 
-const fecha1 = new Date(2021,1,28)
-const fecha2 = new Date(fecha1.getFullYear(), fecha1.getMonth(), fecha1.getDate()+15)
+const fechaInicio = process.argv[2]
+const fechaFin = process.argv[3]
+
+const fecha1 = new Date(fechaInicio)
+const fecha2 = new Date(fechaFin)
 
 const cumpleanios = []
 
-// Enumero a los cumpleañeros y muestro solo su fecha de nacimiento y su nombre
-const mostrar = (datos) =>{
+function isValidDate(date) {
+    let regex = new RegExp(/^([12]\d{3}\/(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01]))$/)
+    let dateOk = regex.test(date)
+    return dateOk
+}
+if (isValidDate(fechaInicio) && isValidDate(fechaFin)) {
+    console.log('Formato correcto')
+
+} else if (!isValidDate(fechaInicio)) {
+    console.log('Formato incorrecto de la primera fecha ingresada')
+    return false
+} else if (!isValidDate(fechaFin)) {
+    console.log('Formato incorrecto de la segunda fecha ingresada')
+    return false
+} else {
+    console.log('Formato de ambas fechas es invalido')
+    return false
+}
+const mostrar = (datos) => {
     for (i in datos){
-        var a=parseInt(i)+1
+        const a = parseInt(i) + 1
         console.log(a,".- ",datos[i].cumpleanios, datos[i].apellido_y_nombre)
     }
 }
-
-// Recibo ambas fechas en formato Date() mas el cumpleaños en formato de lista [aaaa,mm,dd], 
-// retorno true si el cumpleaños esta entre la fecha1 y la fecha2
-// retorno false en otro caso
 const compararFecha = (fecha1, fecha2, cumple) => {
     mesCumple = parseInt(cumple[1])
     diaCumple= parseInt(cumple[2])
-    if(mesCumple == fecha1.getMonth()+1 && mesCumple == fecha2.getMonth()+1){
+    if(mesCumple == fecha1.getMonth() + 1 && mesCumple == fecha2.getMonth() + 1){
         if (diaCumple >= fecha1.getDate() && diaCumple <= fecha2.getDate()){
             return true
         }
     }
-    else if(mesCumple === fecha1.getMonth()+1){
+    else if(mesCumple === fecha1.getMonth() + 1){
         if(diaCumple >= fecha1.getDate()){
             return true
         }
-    }else if (mesCumple === fecha2.getMonth()+1){
+    }else if (mesCumple === fecha2.getMonth() + 1){
         if(diaCumple <= fecha2.getDate()){
             return true
         }
+    } else if (mesCumple > fecha1.getMonth() + 1 && mesCumple < fecha2.getMonth() + 1) {
+        return true
     }
     return false
 }
-
-// Leo el archivo .csv y guardo solo los empleados que estan de cumpleaños
-// desde hoy hasta 15 dias mas
 fs.createReadStream('mails_y_cumples_03.csv')
     .pipe(parse({
             delimiter: ','
