42dca1ac913e7006d1d2f082688b30f588cdbde1
Beatriz <lopezcasanovabeatriz@gmail.com>
2022-02-14 19:46:44 -0300

    agregando

---
+++ src/menu.js
@@ -0,0 +1,4 @@
+Options:
+// 1. Insert a starting and ending date and obtain the birthdays between those dates
+// 2. Obtain the current month birthdays.
+// 3. Obtain the following month birthdays.
