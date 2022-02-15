6f01106dfafb93200a799497f76eda8d1703f03e
KiraElric <lissette.urso@gmail.com>
2022-02-15 13:35:06 -0300

    Refactor: Modularizacion de funciones en archivos individuales

---
+++ src/reader.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+function dataFile(fileName) {
+  // eslint-disable-next-line consistent-return
+  const data = fs.readFileSync(fileName, 'utf-8', (err, file) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    } else {
+      return file;
+    }
+  });
+  return data;
+}
+
+module.exports = { dataFile };
