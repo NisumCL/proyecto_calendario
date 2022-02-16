dc18fc2a37bf495687a25c230bbecbd0b3102bd1
Lissette <62615474+KiraElric@users.noreply.github.com>
2022-02-16 18:35:46 -0300

    Lurso v3 (#13)
    * eslint/prettier/airbnb node code standard
    
    * eslint/prettier/airbnb node code standard
    
    * Refactor: Modularizacion de funciones en archivos individuales
    
    * Refactor: Funcion para convertir la data ingresada por consola en tipo fecha, creacion de funcion que compara que fecha es mayor
    
    * Update src/app.js
    
    no se que sentido tendria hacer eso si al momento de utilizar el parametro dentro el date lo convierte a objeto date y no serviria el isValidDateFormat el cual es un regex de texto. Ademas el !isNaN no veo que haga nada util dado que el tipo del date es object.
    
    Co-authored-by: Anggelo Urso <anggelo.urso@gmail.com>
    
    * Change the name of the params in the function convertToDate
    
    * Refactor: Funcion comparar fecha subdividido en funcion transformar a fecha, comparar fechas ingresadas y filtrar cumpleaños
    
    * Add .idea to gitignore
    
    Co-authored-by: Anggelo Urso <anggelo.urso@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,79 +1,32 @@
-require("dotenv").config();
+const { dataFile } = require('./reader');
+const { dataToObject, show, convertToDate } = require('./converter');
+const { isValidDateFormat } = require('./validator');
+const { biggerDate, filteredBirthdays } = require('./comparator');
 
-const parse = require("csv-parser");
-const fs = require("fs");
-
-const fechaInicio = process.argv[2];
-const fechaFin = process.argv[3];
-
-const fecha1 = new Date(fechaInicio);
-const fecha2 = new Date(fechaFin);
-
-const cumpleanios = [];
-
-
-function isValidDate(date) {
-    let regex = new RegExp(
-        /^([12]\d{3}\/(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01]))$/
-    );
-    let dateOk = regex.test(date);
-    return dateOk;
-}
-if (isValidDate(fechaInicio) && isValidDate(fechaFin)) {
-    console.log("Formato correcto");
-} else if (!isValidDate(fechaInicio)) {
-    console.log("Formato incorrecto de la primera fecha ingresada");
-    return false;
-} else if (!isValidDate(fechaFin)) {
-    console.log("Formato incorrecto de la segunda fecha ingresada");
-    return false;
-} else {
-    console.log("Formato de ambas fechas es invalido");
-    return false;
-}
-const mostrar = (datos) => {
-    for (i in datos) {
-        const a = parseInt(i) + 1;
-        console.log(a, ".- ", datos[i].cumpleanios, datos[i].apellido_y_nombre);
+try {
+  const startDate = process.argv[2];
+  const endDate = process.argv[3];
+  let birthdayList = [];
+  if (isValidDateFormat(startDate) && isValidDateFormat(endDate)) {
+    const firstDate = convertToDate(startDate);
+    const secondDate = convertToDate(endDate);
+    const fileInfo = dataFile('./mails_y_cumples_03.csv');
+    const workersData = dataToObject(fileInfo);
+    if (biggerDate(firstDate, secondDate)) {
+      birthdayList = filteredBirthdays(firstDate, secondDate, workersData);
+    } else {
+      const startYear = new Date();
+      startYear.setMonth(0, 1);
+      const endYear = new Date();
+      endYear.setMonth(11, 31);
+      birthdayList = [
+        ...filteredBirthdays(firstDate, endYear, workersData),
+        ...filteredBirthdays(startYear, secondDate, workersData),
+      ];
     }
-};
-const compararFecha = (fecha1, fecha2, cumple) => {
-    mesCumple = parseInt(cumple[1]);
-    diaCumple = parseInt(cumple[2]);
-    if (
-        mesCumple == fecha1.getMonth() + 1 &&
-        mesCumple == fecha2.getMonth() + 1
-    ) {
-        if (diaCumple >= fecha1.getDate() && diaCumple <= fecha2.getDate()) {
-            return true;
-        }
-    } else if (mesCumple === fecha1.getMonth() + 1) {
-        if (diaCumple >= fecha1.getDate()) {
-            return true;
-        }
-    } else if (mesCumple === fecha2.getMonth() + 1) {
-        if (diaCumple <= fecha2.getDate()) {
-            return true;
-        }
-    } else if (
-        mesCumple > fecha1.getMonth() + 1 &&
-        mesCumple < fecha2.getMonth() + 1
-    ) {
-        return true;
-    }
-    return false;
-};
-fs.createReadStream("mails_y_cumples_03.csv")
-    .pipe(
-        parse({
-            delimiter: ",",
-        })
-    )
-    .on("data", (dataRow) => {
-        if (compararFecha(fecha1, fecha2, dataRow.cumpleanios.split("-"))) {
-            cumpleanios.push(dataRow);
-        }
-    })
-    .on("end", () => {
-        mostrar(cumpleanios);
-    });
+    show(birthdayList);
+  }
+} catch (e) {
+  // eslint-disable-next-line no-console
+  console.log(e.message);
+}
---
+++ src/comparator.js
@@ -0,0 +1,12 @@
+function biggerDate(firstDate, secondDate) {
+  const differenceDate = secondDate - firstDate;
+  return differenceDate >= 0;
+}
+
+function filteredBirthdays(firstDate, secondDate, workersData) {
+  return workersData.filter(
+    worker => worker.birthday >= firstDate && worker.birthday <= secondDate
+  );
+}
+
+module.exports = { biggerDate, filteredBirthdays };
