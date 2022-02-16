2785b8ca85e01c070ccc45f76ead150e27873eab
Francisca <franciscaljubetic@gmail.com>
2022-02-16 12:48:56 -0300

    tests ok

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Proyecto Calendario - Aplicación retorna cumpleaños que se encuentren en un margen de dos fechas ingresadas. ejemplo: 14 de Febrero y 20 de Febrero.
 
 Versión número 3, Test unitarios - Check list de condiciones: https://docs.google.com/spreadsheets/d/1I7aBDzC6I0O-W6QpJEyLQDXhd3duoVJu4fJ9HEYvQEI/edit?usp=sharing
+
+
+Esta rama corresponde al test del trabajo de refactorizacion que hicieron Lissette y javiera en la rama lurso-v3
--- src/app.js
+++ src/app.js
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-const parse = require("csv-parser");
-const fs = require("fs");
-const test = require("./test");
-const show = require("./show");
-
-const inputInicio = process.argv[2];
-const inputFin = process.argv[3];
-
-const cumpleanios = [];
-
-const isValidInput = test.isInformedInput(inputInicio, inputFin);
-
-const isValidInputInicioFormatDate = test.isValidFormatDate(inputInicio);
-
-const isValidInputFinFormatDate = test.isValidFormatDate(inputFin);
-
-if (!isValidInputInicioFormatDate || !isValidInputFinFormatDate) {
-    console.log('Debes ingresar el formato de fecha: "YYYY/MM/DD"');
-    return false;
-}
-
-if (isValidInput) {
-    const startDate = new Date(inputInicio);
-    const finishDate = new Date(inputFin);
-
-    let areValidDates = test.isValidDate(startDate, finishDate);
-
-    if (areValidDates) {
-        fs.createReadStream("mails_y_cumples_03.csv")
-            .pipe(
-                parse({
-                    delimiter: ",",
-                })
-            )
-            .on("data", (dataRow) => {
-                if (
-                    test.compararFecha(
-                        startDate,
-                        finishDate,
-                        dataRow.cumpleanios.split("-")
-                    )
-                ) {
-                    cumpleanios.push(dataRow);
-                }
-            })
-            .on("end", () => {
-                show.mostrar(cumpleanios);
-            });
-    }
-
-    let mostrarData = show.mostrar;
-
-    let compararDate = test.compararFecha;
-=======
 const { dataFile } = require('./reader');
 const { dataToObject, show, convertToDate } = require('./converter');
 const { isValidDateFormat } = require('./validator');
@@ -84,5 +29,4 @@ try {
 } catch (e) {
   // eslint-disable-next-line no-console
   console.log(e.message);
->>>>>>> b8212c845e85b17194e7b3f539d7ca7c4d070f0e
 }
