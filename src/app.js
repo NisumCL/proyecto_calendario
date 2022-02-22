a39de6ef59a3cba3d568d440f246839d32e94b89
Kevin Genskowsky Jeanneret <kgenskowsky@gmail.com>
2022-02-22 10:03:37 -0300

    push v0.1

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const { dataFile } = require('./reader');
 const { dataToObject } = require('./converter');
-const { principalMain } = require('./options')
+const { principalMain } = require('./options');
+
 
 try {
   const fileInfo = dataFile('./mails_y_cumples_03.csv');
   const workersData = dataToObject(fileInfo);
-  principalMain(workersData)
-}catch (e) {
-  // eslint-disable-next-line no-console
+  principalMain(workersData);
+} catch (e) {
+    // eslint-disable-next-line no-console
   console.log(e.message);
 }
+
+// git config --global core.autocrlf true
