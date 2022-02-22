6b90c33b1fd86b7892179f56f760d5e1e6f804d1
Kevin Genskowsky Jeanneret <kgenskowsky@gmail.com>
2022-02-22 10:56:03 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
+/* eslint-disable */
 const { dataFile } = require('./reader');
 const { dataToObject } = require('./converter');
 const { principalMain } = require('./options');
 
-
-try {
   const fileInfo = dataFile('./mails_y_cumples_03.csv');
   const workersData = dataToObject(fileInfo);
-  principalMain(workersData);
-} catch (e) {
-    // eslint-disable-next-line no-console
-  console.log(e.message);
-}
+
+  (async () => {
+    while(true){
+      console.log('inicio automatico ');
+      const r = await principalMain(workersData);
+      if (r == true){
+        break;
+      }
+
+    }
+    console.log('FIN');
+  })();
+
+ 
+
 
 // git config --global core.autocrlf true
--- src/options.js
+++ src/options.js
@@ -14,9 +14,10 @@ const readline = require('readline').createInterface({
 const buscarEntreFechasPromesa = () => {
   return new Promise((resolve, reject) => {
     readline.question('Ingrese fecha de inicio (yyyy/mm/dd): ', (startDate) => {
-      if (startDate.includes('a')) {
-        return reject('WEIRD date');
-      }
+      // ! HACER REGEX 
+        if (isValidDateFormat(startDate)) {
+          return reject('Fecha no cumple formato');
+        }
       resolve(startDate);
     });
   });
@@ -25,6 +26,9 @@ const buscarEntreFechasPromesa = () => {
 const ingresarFechaFinPromesa = () => {
   return new Promise((resolve, reject) => {
     readline.question('Ingrese fecha de fin (yyyy/mm/dd): ', (endDate) => {
+      if (isValidDateFormat(startDate)) {
+        return reject('Fecha no cumple formato');
+      }
       resolve(endDate);
     });
   });
@@ -47,35 +51,32 @@ function obtenerFechaFin(startDate, workersData) {
 
 function obtenerCumples(startDate, endDate, workersData) {
   let birthdayList = [];
-  if (isValidDateFormat(startDate) && isValidDateFormat(endDate)) {
-    const firstDate = convertToDate(startDate);
-    const secondDate = convertToDate(endDate);
-    if (biggerDate(firstDate, secondDate)) {
-      birthdayList = filteredBirthdays(firstDate, secondDate, workersData);
-    } else {
-      const startYear = new Date();
-      startYear.setMonth(0, 1);
-      const endYear = new Date();
-      endYear.setMonth(11, 31);
-      birthdayList = [
-        ...filteredBirthdays(firstDate, endYear, workersData),
-        ...filteredBirthdays(startYear, secondDate, workersData),
-      ];
-    }
-    show(birthdayList);
-  }
+  // if (isValidDateFormat(startDate) && isValidDateFormat(endDate)) {
+  //   const firstDate = convertToDate(startDate);
+  //   const secondDate = convertToDate(endDate);
+  //   if (biggerDate(firstDate, secondDate)) {
+  //     birthdayList = filteredBirthdays(firstDate, secondDate, workersData);
+  //   } else {
+  //     const startYear = new Date();
+  //     startYear.setMonth(0, 1);
+  //     const endYear = new Date();
+  //     endYear.setMonth(11, 31);
+  //     birthdayList = [
+  //       ...filteredBirthdays(firstDate, endYear, workersData),
+  //       ...filteredBirthdays(startYear, secondDate, workersData),
+  //     ];
+  //   }
+  //   show(birthdayList);
+  // }
 }
 
-function buscarMesActual(workersData) {
+function buscarMesActual (workersData) {
   let birthdayList = [];
   const today = new Date();
   const firstDate = new Date(2022, today.getMonth(), 1);
   const secondDate = new Date(2022, today.getMonth() + 1, 0);
-  birthdayList = filteredBirthdays(firstDate, secondDate, workersData);
-  show(birthdayList);
-  readline.question('\nPresione ENTER para continuar', (aux) => {
-    principalMain(workersData);
-  });
+  
+  obtenerCumples(firstDate,secondDate,workersData);
 }
 
 function buscarMesSiguiente(workersData) {
@@ -83,54 +84,91 @@ function buscarMesSiguiente(workersData) {
   const today = new Date();
   const firstDate = new Date(2022, today.getMonth() + 1, 1);
   const secondDate = new Date(2022, today.getMonth() + 2, 0);
-  birthdayList = filteredBirthdays(firstDate, secondDate, workersData);
-  show(birthdayList);
-  readline.question('\nPresione ENTER para continuar', (aux) => {
-    principalMain(workersData);
-  });
+  
+  obtenerCumples(firstDate, secondDate, workersData);
+
 }
 
-function principalMain(workersData) {
+const menuPromesa = () => {
+  return new Promise((resolve, reject) => {
+    readline.question('Seleccione una opcion: ', (menu) => {
+      resolve(menu);
+    });
+  });
+};
+
+const enterPromesa = () => {
+  return new Promise((resolve, reject) => {
+    readline.question('Presione cualquiercosa para continuar: ', (enter) => {
+      resolve(enter);
+    });
+  });
+};
+
+const fechasManual = async () =>{
+  try {
+    const fechaStart = await buscarEntreFechasPromesa();
+  
+    const date1 = new Date(fechaStart);
+  
+    const fechaFin = await ingresarFechaFinPromesa();
+
+    console.log(`-> ${fechaStart}   ${fechaFin}`);
+
+    obtenerCumples(fechaStart, fechaFin, workersData);
+
+    await enterPromesa();
+
+  } catch (error) {
+    await enterPromesa();
+    console.error('error->', error);
+  }
+  return;
+}
+
+
+const principalMain = async (workersData) => {
   printMain();
-  readline.question('Seleccione una opcion: ', async (option) => {
-    const opt = parseInt(option);
-    switch (opt) {
-      case 1:
-        //buscarEntreFechasPromesa(workersData).then((valor)=>{}).catch((error) => console.error('error->',error));
-        try {
-          const fechaStart = await buscarEntreFechasPromesa();
-          const fechaFin = await ingresarFechaFinPromesa();
 
-          console.log(`-> ${fechaStart}   ${fechaFin}`);
+  const selectorMenu = await menuPromesa();
+  const opt = parseInt(selectorMenu);
 
-          obtenerCumples(fechaStart, fechaFin, workersData);
 
-        
-        } catch (error) {
-          console.error('error->', error);
-        }
+  switch (opt) {
+    case 1:
+      //buscarEntreFechasPromesa(workersData).then((valor)=>{}).catch((error) => console.error('error->',error));
+      await fechasManual();
+      break;
+    case 2:
+      try {
+        console.log('CASE 2');
 
-        // buscarEntreFechas(workersData);
-        return;
-      case 2:
-        buscarMesActual(workersData);
-        return;
-      case 3:
-        buscarMesSiguiente(workersData);
-        return;
-      case 0:
-        console.log('\nGracias por usar el programa.\n');
-        readline.close();
-        return;
-      default:
-        console.log('seleccione opcion valida');
-        readline.question('\nPresione ENTER para continuar', (aux) => {
-          principalMain(workersData);
-        });
-        return;
-    }
-  });
-}
+        const fechaStart = new Date().setDate(1);
+        const fechaFin =  new Date().setDate(31);
+
+        console.log(`-> ${fechaStart}   ${fechaFin}`);
+
+        obtenerCumples(fechaStart, fechaFin, workersData);
+        await enterPromesa();
+
+      } catch (error) {
+        console.error('error->', error);
+      }
+    case 3:
+      buscarMesSiguiente(workersData);
+      return;
+    case 0:
+      console.log('\nGracias por usar el programa.\n');
+      readline.close();
+      return true;
+    default:
+      console.log('seleccione opcion valida');
+      // 
+  }
+
+  throw  new Error('FIN SWITCH CASE');
+  return;
+};
 
 module.exports = {
   principalMain,
