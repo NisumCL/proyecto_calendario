bc5c550dd8d4263c7673fa6b11f01a7c3b7c717b
Gabriela Castro <gaby.castro@outlook.com>
2022-02-24 18:45:32 -0300

    metodo get creado y borre funciones que ya no se utilizan

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
-const { dataFile } = require('./reader');
-const { dataToObject } = require('./converter');
-const { principalMain } = require('./options')
+const express = require('express');
+const { dataFile } = require('./utils/reader');
+const { getBirthdaysFromStrings } = require('./utils/options');
+const { dataToObject } = require('./utils/converter');
+const { isValidDateFormat } = require('./utils/validator');
 
-try {
-  const fileInfo = dataFile('./mails_y_cumples_03.csv');
-  const workersData = dataToObject(fileInfo);
-  principalMain(workersData)
-}catch (e) {
-  // eslint-disable-next-line no-console
-  console.log(e.message);
-}
+const workersData = dataToObject(dataFile('./mails_y_cumples_03.csv'));
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
+
+app.get('/birthday_between_dates', (req, res) => {
+  const { startDate } = req.query;
+  const { endDate } = req.query;
+
+  if (!startDate || !isValidDateFormat(startDate)) {
+    res.send({ error: 'Problems with startDate' });
+  }
+  if (!endDate || !isValidDateFormat(endDate)) {
+    res.send({ error: 'Problems with endDate' });
+  }
+  const birthdays = getBirthdaysFromStrings(startDate, endDate, workersData);
+  res.send(birthdays);
+});
--- src/utils/reader.js
+++ src/utils/reader.js
@@ -4,7 +4,6 @@ function dataFile(fileName) {
   // eslint-disable-next-line consistent-return
   const data = fs.readFileSync(fileName, 'utf-8', (err, file) => {
     if (err) {
-      // eslint-disable-next-line no-console
       console.error(err);
     } else {
       return file;
