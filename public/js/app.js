70780d67d065f81292b1c32e57b3d7c7661fbcf0
Francisca <franciscaljubetic@gmail.com>
2022-02-25 12:09:24 -0300

    guardar

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ searchinrangeForm.addEventListener('submit', (e) =>{
     messageOne.textContent = `Loading...`
     messageTwo.textContent = ``
 
-    fetch(`/theDates?stardate=${startDate}&${endDate}`).then((response)=>{
+    fetch(`/birthday_month_course?stardate=${startDate}&${endDate}`).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                 messageOne.textContent = `${data.error}`
@@ -31,19 +31,6 @@ searchinrangeForm.addEventListener('submit', (e) =>{
 })
 
 
-// getOptions.addEventListener('submit', (e) =>{
-//     e.preventDefault()
-//     const option = optrange.value
-    
-//     messageOne.textContent = `Loading...`
-//     messageTwo.textContent = ``
-//     // if option === '1' => la ruta es con option 1, se muestra o desbloquea el calendario y se pasa a la segunda ruta
-//     // if option === '2' => la ruta es con option 2 y devuelve lo que devuelve la app cuando le entregan 2
-//     // if option === '2' => la ruta es con option 3 y devuelve lo que devuelve la app cuando le entregan 3
-//     fetch(`/Dates?stardate=${option}`).then((response)=>{
-//         //if
-//     })
-// })
 
 
 
