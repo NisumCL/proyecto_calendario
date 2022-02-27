503b093c305adeb85f68b49b3fb9835e2af0b5dc
Javiera Morales Santander <javi@MBP-de-Javiera.Home>
2022-02-27 16:20:15 -0300

    guardar

--- src/app.js
+++ src/app.js
@@ -1,9 +1,45 @@
+const path = require('path');
+const hbs = require('hbs');
 const express = require('express');
 const { actualMonthService, nextMonthService, betweenTwoDatesService } = require('./utils/service');
 const { isValidDateFormat } = require('./utils/validator');
 
 const app = express();
 const port = 3000;
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Birthday app',
+  });
+});
+
+app.get('/currentMonth', (req, res) => {
+  res.render('currentMonth', {
+    title: 'Current Month',
+  });
+});
+
+app.get('/nextMonth', (req, res) => {
+  res.render('nextMonth', {
+    title: 'Next Month',
+  });
+});
+
+app.get('/betweenDates', (req, res) => {
+  res.render('betweenDates', {
+    title: 'Between Dates',
+  });
+});
 
 app.get('/birthday_month_course', (req, res) => {
   const birthdays = actualMonthService();
@@ -26,6 +62,13 @@ app.get('/birthday_between_dates', (req, res) => {
   }
   const birthdays = betweenTwoDatesService(startDate, endDate);
   res.send(birthdays);
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Page not found.',
+  });
 });
 
 app.listen(port, () => {
