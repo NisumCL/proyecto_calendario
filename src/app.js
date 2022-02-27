9ea2032fbd476238dd2fbf5a04c63b3c755cd841
Francisca <franciscaljubetic@gmail.com>
2022-02-27 03:20:31 -0300

    se borrar modulos innecesarios

--- src/app.js
+++ src/app.js
@@ -2,11 +2,8 @@
 const path= require('path');
 const express = require('express')
 const hbs = require('hbs')
-const parser = require('body-parser')
 const { actualMonthService, nextMonthService, betweenTwoDatesService } = require('./utils/service');
 const { isValidDateFormat } = require('./utils/validator');
-
-const urlencodedParser = parser.urlencoded({ extended: true })
 
 const app = express()
 // const port = process.env.PORT || 3000
@@ -16,8 +13,6 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-app.use(urlencodedParser)
-app.use(parser.json())
 //set up handlebars engine and views locations
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
@@ -33,29 +28,11 @@ app.get('', (req, res) =>{
     
 })
 
-// app.get('/thismonth', (req, res) =>{
-
-//     res.render('thismonth', {
-//         title: 'Birthdays in the month',
-//     })
-// })
-
-// app.get('/nextmonth', (req, res) =>{
-//     res.render('nextmonth', {
-//         title: 'Birthdays in the next month',
-//     })
-// })
-
-// app.get('/searchinrange', (req, res) =>{
-//     res.render('search', {
-//         title: 'NISUM Birthdays',
-//     })
-// })
-
 app.get('/birthday_month_course', (req, res) => {
     const birthdays = actualMonthService();
     res.send(birthdays);
 });
+
 
 app.get('/birthday_next_month', (req, res) => {
     const birthdays = nextMonthService();
@@ -79,8 +56,6 @@ app.get('/birthday_between_dates', (req, res) => {
 app.get('*', function(req, res){
     res.render('errorpage');
 });
-
-
 
 app.listen(3000);
 // app.listen(port, () =>{
