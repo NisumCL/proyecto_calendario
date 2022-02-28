28ae0dba7e3177c65f460f322acaa9d19ec7488b
Javiera Morales Santander <javi@MBP-de-Javiera.Home>
2022-02-28 16:46:30 -0300

    current month

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,27 @@
 /* eslint-disable */
-/* eslint-disable indent */
-/* eslint-disable prettier/prettier */
-function loadData(data){
-  $(".table-results").append('<tr><td>NAME</td>'+
-  '<td>LAST NAME</td>'+
-  '<td>BIRTHDAY</td>' + 
-  '<td>EMAIL</td>');
-  for (i = 0; i < data.length; i ++){
+function loadData(data) {
+  $('.table').append(
+    '<tr><td>NAME</td>' + '<td>LAST NAME</td>' + '<td>BIRTHDAY</td>' + '<td>EMAIL</td>');
 
-$(".table-results").append('<tr>' + 
-  '<td align="center" style="dislay: none;">' + data[i].name + '</td>'+
-  '<td align="center" style="dislay: none;">' + data[i].lastname + '</td>'+
-  '<td align="center" style="dislay: none;">' + data[i].birthday + '</td>'+
-  '<td align="center" style="dislay: none;">' + data[i].email + '</td>'+'</tr>');
+  for (let i = 0; i < data.length; i++) {
+    $(".table").append('<tr>' + 
+    '<td align="center" style="dislay: none;">' + data[i].name + '</td>'+
+    '<td align="center" style="dislay: none;">' + data[i].lastname + '</td>'+
+    '<td align="center" style="dislay: none;">' + data[i].birthday + '</td>'+
+    '<td align="center" style="dislay: none;">' + data[i].email + '</td>'+'</tr>');
   }
 }
 
-const currentMonthForm = document.querySelector('.form1');
-
-currentMonthForm.addEventListener('click', (e) => {
-    e.preventDefault();
-    fetch('/birthday_month_course').then((response) => {
-      response.json().then((data) => {
-          if (data.error) {
-              console.log(data.error);
-          } else {
-              loadData(data);
-          }
-      });
+const form = document.querySelector('form');
+form.addEventListener('click', e => {
+  e.preventDefault();
+  fetch('/birthday_month_course').then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        loadData(data);
+      }
+    });
   });
-
 });
-
