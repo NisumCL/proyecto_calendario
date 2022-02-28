31d4f3932cbb4bafa0f102ec4b7bd1af4a6e0ddb
Javiera Morales Santander <javi@MBP-de-Javiera.Home>
2022-02-27 23:22:09 -0300

    styles.css-4

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,33 @@
-/* eslint-disable no-undef */
+/* eslint-disable */
 /* eslint-disable indent */
 /* eslint-disable prettier/prettier */
-//traerse los datos!
+function loadData(data){
+  $(".table-results").append('<tr><td>NAME</td>'+
+  '<td>LAST NAME</td>'+
+  '<td>BIRTHDAY</td>' + 
+  '<td>EMAIL</td>');
+  for (i = 0; i < data.length; i ++){
 
+$(".table-results").append('<tr>' + 
+  '<td align="center" style="dislay: none;">' + data[i].name + '</td>'+
+  '<td align="center" style="dislay: none;">' + data[i].lastname + '</td>'+
+  '<td align="center" style="dislay: none;">' + data[i].birthday + '</td>'+
+  '<td align="center" style="dislay: none;">' + data[i].email + '</td>'+'</tr>');
+  }
+}
 
-const resultsTable = document.querySelector('form');
+const currentMonthForm = document.querySelector('form');
 
-resultsTable.addEventListener('click', (e) => {
+currentMonthForm.addEventListener('click', (e) => {
     e.preventDefault();
+    fetch('/birthday_month_course').then((response) => {
+      response.json().then((data) => {
+          if (data.error) {
+              alert(`${data.error}`);
+          } else {
+              loadData(data);
+          }
+      });
+  });
 
-    console.log('testing!');
-
-})
-// eslint-disable-next-line no-undef
-fetch('/birthday_month_course').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data);
-        }
-    });
 });
