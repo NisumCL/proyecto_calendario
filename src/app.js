a9468b325e52d5d08e2e274df42a80728dff726a
Gabriela <67624352+melamito@users.noreply.github.com>
2022-03-08 10:20:24 -0300

    Refactorización del código y menú combinados y funcionando (#22)
    * Refactorizacion y menu
    
    * Quitar funciones que se importaban y no se usaban
    
    * Cambios solicitados

--- src/app.js
+++ src/app.js
@@ -1,32 +1,12 @@
 const { dataFile } = require('./reader');
-const { dataToObject, show, convertToDate } = require('./converter');
-const { isValidDateFormat } = require('./validator');
-const { biggerDate, filteredBirthdays } = require('./comparator');
+const { dataToObject } = require('./converter');
+const { main } = require('./options')
 
 try {
-  const startDate = process.argv[2];
-  const endDate = process.argv[3];
-  let birthdayList = [];
-  if (isValidDateFormat(startDate) && isValidDateFormat(endDate)) {
-    const firstDate = convertToDate(startDate);
-    const secondDate = convertToDate(endDate);
-    const fileInfo = dataFile('./mails_y_cumples_03.csv');
-    const workersData = dataToObject(fileInfo);
-    if (biggerDate(firstDate, secondDate)) {
-      birthdayList = filteredBirthdays(firstDate, secondDate, workersData);
-    } else {
-      const startYear = new Date();
-      startYear.setMonth(0, 1);
-      const endYear = new Date();
-      endYear.setMonth(11, 31);
-      birthdayList = [
-        ...filteredBirthdays(firstDate, endYear, workersData),
-        ...filteredBirthdays(startYear, secondDate, workersData),
-      ];
-    }
-    show(birthdayList);
-  }
-} catch (e) {
+  const fileInfo = dataFile('./mails_y_cumples_03.csv');
+  const workersData = dataToObject(fileInfo);
+  main(workersData)
+}catch (e) {
   // eslint-disable-next-line no-console
   console.log(e.message);
 }
---
+++ src/main.js
@@ -0,0 +1,12 @@
+function printMenu () {
+    console.clear()
+    console.log('-------- MENU --------')
+    console.log('1. Obtain the birthdays between two dates.')
+    console.log('2. Obtain the current month birthdays.')
+    console.log('3. Obtain the following month birthdays.')
+    console.log('0. Exit.\n')
+  }
+  
+  module.exports = {
+    printMenu
+  }
